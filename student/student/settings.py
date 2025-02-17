e2e362f89e313c6dcbf8e5045302baf8099267ae
Opp36 <120030071+Opp36@users.noreply.github.com>
2025-02-17 23:02:01 +0530

    Update settings.py
    debug false and host changes

--- student/student/settings.py
+++ student/student/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!cuxkboyce)p0l+-zhxeunc#-9k!*gtue5h@7tb4%ljv@+=8ij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
